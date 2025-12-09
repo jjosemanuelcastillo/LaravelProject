dd1c6c9321607ce7310ecc9321f13faa1379f218
José Manuel Castillo Calle <josemanuelcastillocalle83@gmail.com>
2025-12-09 20:59:53 +0100

    cambios

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://angularproject-6elskjpeq-jjosemanuelcastillos-projects.vercel.app'],
+    'allowed_origins' => ['https://angularproject-tan.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
